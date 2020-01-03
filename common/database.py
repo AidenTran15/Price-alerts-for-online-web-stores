6b0832c41404984a2fd24a215b49d9d3b8224527
AidenTran15 <aidenkiettran@gmail.com>
2020-01-03 16:47:30 +1100

    Finishing up the Model superclass

--- common/database.py
+++ common/database.py
@@ -17,3 +17,9 @@ class Database:
     @staticmethod
     def find_one(collection: str, query: Dict) -> Dict:
         return Database.DATABASE[collection].find_one(query)
+
+    @staticmethod
+    def update(collection: str, query: Dict, data: Dict) -> None:
+        Database.DATABASE[collection].update(query, data, upsert=True)
+
+        
--- models/model.py
+++ models/model.py
@@ -9,6 +9,16 @@ class Model(metaclass=ABCMeta):
     def __init__(self, *args, **kwargs):
         pass
 
+    def save_to_mongo(self):
+        Database.update(self.collection, {'_id': self._id}, self.json())
+
+    def remove_from_mongo(self):
+        Database.remove(self.collection, {"_id": self._id})
+
+    @classmethod
+    def get_by_id(cls, _id:str):
+        return cls.find_one_by("_id", id)
+
     @abstractmethod
     def json(self):
         raise NotImplementedError
@@ -17,6 +27,14 @@ class Model(metaclass=ABCMeta):
     def all(cls) -> List:
         elements_from_db = Database.find(cls.collection, {})
         return [cls(**elem) for elem in elements_from_db]
+        
+    @classmethod
+    def fine_one_by(cls, attribute, value): # Item.find_one_by('url', 'https://bla.com')
+        return cls(**Database.find_one(cls.collection, {attribute: value}))
+
+    @classmethod
+    def find_many_by(cls, attribute, value):
+        return[cls(**elem) for elem in Database.find(cls.collection, {attribute: value})]
         
 
 
