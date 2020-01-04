95b27ad1ca2e8373efa846126a45518449f508b9
AidenTran15 <aidenkiettran@gmail.com>
2020-01-05 00:48:41 +1100

    Creating alerts through the web interface

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-from flask import Flask, render_template, request
-from models.item import Item
-import json
+from flask import Flask
+from views.items import item_blueprint
+from views.alerts import alert_blueprint
+
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def new_item():
-    if request.method == 'POST':
-        url = request.form['url']
-        tag_name = request.form['tag_name']
-        query = json.loads(request.form['query'])
-
-        Item(url, tag_name, query).save_to_mongo()
-    return render_template('new_item.html')
+app.register_blueprint(item_blueprint, url_prefix="/items")
+app.register_blueprint(alert_blueprint, url_prefix="/alerts")
 
 if __name__ == '__main__':
     app.run(debug=True)
