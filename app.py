322ef871ce41bd4aabc16c26eb7609ba9780836e
AidenTran15 <aidenkiettran@gmail.com>
2020-01-04 11:10:23 +1100

    Creating items through the web interface

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from models.alert import Alert
+from flask import Flask, render_template, request
+from models.item import Item
+import json
 
-alert = Alert("17a31611bcec46dda7c71df1a60f3a17", 1300)
-alert.save_to_mongo()
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def new_item():
+    if request.method == 'POST':
+        url = request.form['url']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
+
+        Item(url, tag_name, query).save_to_mongo()
+    return render_template('new_item.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
