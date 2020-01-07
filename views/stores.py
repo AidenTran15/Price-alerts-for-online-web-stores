8436ec075a8927654a8586945d66dd3cf30f2cc8
AidenTran15 <aidenkiettran@gmail.com>
2020-01-07 21:59:16 +1100

    Creating stores through the web interface

---
+++ views/stores.py
@@ -0,0 +1,19 @@
+import json
+from Flask import Blueprint, render_template, request, url_for
+from models.store import Store
+
+store_blueprint = Blueprint('stores', __name__)
+
+
+@store_blueprint.route('/')
+def index():
+    stores = Store.all()
+    return render_template('stores/store_index.html', stores=stores)
+
+@store_blueprint.route('/new', methods=['GET', 'POST'])
+def create_store():
+    if request.method == 'POST':
+        name = request.form['name']
+        url_prefix = request.form['url_prefix']
+        tag_name = request.form['tag_name']
+        
