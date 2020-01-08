22d62f20e9054f3bc6fa16ff978f22915c7e22bc
AidenTran15 <aidenkiettran@gmail.com>
2020-01-08 13:22:26 +1100

    Editing stores

--- views/stores.py
+++ views/stores.py
@@ -1,5 +1,5 @@
 import json
-from Flask import Blueprint, render_template, request, url_for
+from Flask import Blueprint, render_template, request, url_for, redirect
 from models.store import Store
 
 store_blueprint = Blueprint('stores', __name__)
@@ -10,10 +10,41 @@ def index():
     stores = Store.all()
     return render_template('stores/store_index.html', stores=stores)
 
+
 @store_blueprint.route('/new', methods=['GET', 'POST'])
 def create_store():
     if request.method == 'POST':
         name = request.form['name']
         url_prefix = request.form['url_prefix']
         tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
+
+        Store(name, url_prefix, tag_name, query).save_to_mongo()
+
+    return render_template('stores/new_store.html')
+
+
+@store_blueprint.route('/edit/<string:store_id>', methods=['GET', 'POST'])
+def edit_store(store_id):
+    store = Store.get_by_id(store_id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        url_prefix = request.form['url_prefix']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
+
+        store.name = name
+        store.url_prefix = url_prefix
+        store.tag_name = tag_name
+        store.query = query
+
+        store.save_to_mongo()
+
+        return redirect(url_for('.index'))
+    
+    return render_template('stores/edit_store.html', store=store)
+    
+
+
         
