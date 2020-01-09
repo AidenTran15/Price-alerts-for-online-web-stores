0a51e318f399e6a1325dbf1a77471a8b3ed14e11
AidenTran15 <aidenkiettran@gmail.com>
2020-01-09 20:31:28 +1100

    Update alert

--- views/alerts.py
+++ views/alerts.py
@@ -1,8 +1,8 @@
 import json
 from flask import Blueprint, render_template, request, session, redirect, url_for
 from models.alert import Alert
-from models.store import Store
 from models.item import Item
+from models.store import Store
 
 
 alert_blueprint = Blueprint('alerts', __name__)
